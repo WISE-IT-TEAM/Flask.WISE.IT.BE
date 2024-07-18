05f8ab55e2373d94de702d98a738e6420ac8c1f0
yw5123 <onestar5123@naver.com>
2024-07-18 17:27:51 +0900

    sqool_db API - query 예외처리 추가 및 json 반환값 보완
    - 입력된 query문에 대한 예외처리 구체화 (create_db 함수)
    - status 어미 수정
    - message 추가: 사용자에게 보여줄 안내 메세지

--- app/routes/api/sqool_db.py
+++ app/routes/api/sqool_db.py
@@ -23,7 +23,7 @@ def execute_query_with_rollback(query):
     sqldb_id = session.get("sqldb_id")
 
     if sqldb_id not in db_connections.keys():
-        return jsonify({"status": "DB가 생성되지 않았습니다."}), 400
+        return jsonify({"status": "DB가 생성되지 않았음"}), 400
 
     db = db_connections[sqldb_id]
 
@@ -39,7 +39,7 @@ def execute_query_with_rollback(query):
         )
         db.commit()
 
-        return jsonify({"result": result, "columns": columns}), 200
+        return jsonify({"message": "쿼리가 정상적으로 실행되었습니다.", "result": result, "columns": columns, "status": "쿼리 실행 성공"}), 200
     except sqlite3.Error as e:
         db.rollback()
         raise e
@@ -53,9 +53,9 @@ def create_db():
     dbname = data.get("dbname")
 
     if not dbname:
-        return jsonify({"status": "DB 정보가 없습니다."}), 400
+        return jsonify({"status": "DB 정보가 오지 않음"}), 400
     elif dbname not in DB_CONFIGS.keys():
-        return jsonify({"status": "DB 이름이 올바르지 않습니다."}), 400
+        return jsonify({"status": "DB 이름이 올바르지 않음"}), 400
 
     # 이미 DB가 있을 경우 해당 connection을 삭제 후 DB 생성 (RESET)
     sqldb_id = session.get("sqldb_id")
@@ -75,11 +75,11 @@ def create_db():
             with open(sql_path, "r", encoding="UTF-8") as file:
                 db.executescript(file.read())
         else:
-            print(f"오류: {sql_file} 파일이 존재하지 않습니다.")
+            return jsonify({"status": "{sql_file} 파일이 존재하지 않음"}), 400
 
     db_connections[sqldb_id] = db
 
-    return jsonify({"status": "사용자 데이터베이스가 정상적으로 생성되었습니다."}), 200
+    return jsonify({"status": "사용자 DB 정상적으로 생성"}), 200
 
 
 @sqool_db_bp.route("/schema", methods=["GET"])
@@ -87,7 +87,7 @@ def get_schema():
     sqldb_id = session.get("sqldb_id")
 
     if sqldb_id not in db_connections.keys():
-        return jsonify({"status": "DB가 생성되지 않았습니다."}), 400
+        return jsonify({"status": "DB가 생성되지 않음"}), 400
 
     db = db_connections[sqldb_id]
     cursor = db.cursor()
@@ -118,14 +118,19 @@ def execute_query():
     data = request.json
     query = data.get("query")
 
-    if not query:
-        return jsonify({"status": "쿼리값이 없습니다."}), 400
+    SQL_KEYWORD = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP']
+
+    if not query or query.isspace():
+        return jsonify({"message": "쿼리를 입력해주세요.", "status": "쿼리값이 없음"}), 400
+    
+    if query.split()[0].upper() not in SQL_KEYWORD:
+        return jsonify({"message": "입력값을 확인해주세요.", "status": "SQL_KEYWORD에 해당하지 않은 시작"}), 400
 
     try:
         result = execute_query_with_rollback(query)
         return result
     except sqlite3.Error as e:
-        return jsonify({"status": f"잘못된 요청입니다: {str(e)}"}), 400
+        return jsonify({"message": "입력값을 확인해주세요.", "status": f"잘못된 요청: {str(e)}"}), 400
 
 
 # '/reset' 대신 '/'를 통해 무조건 db를 재생성
