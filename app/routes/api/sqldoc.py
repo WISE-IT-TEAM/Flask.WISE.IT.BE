f7238e8988c98ce2ff002c755f5d9af6beb6897d
yw5123 <onestar5123@naver.com>
2024-07-22 18:40:20 +0900

    sqldoc 카테고리 api 완성

--- app/routes/api/sqldoc.py
+++ app/routes/api/sqldoc.py
@@ -7,3 +7,55 @@ sqldoc_api_bp = Blueprint("sqldoc_api", __name__)
 @sqldoc_api_bp.route("/ping", methods=["GET"])
 def ping():
     return jsonify({"message": "sqldoc API Test Success"})
+
+
+@sqldoc_api_bp.route("/category", methods=["GET"])
+def get_category():
+    # json에 담을 리스트 선언
+    category_list = []
+
+    # 최상위(main) 디렉토리 받아오기
+    main_cats = SqlDocCategory.query\
+            .filter_by(parent_id=None)\
+            .with_entities(SqlDocCategory.id, SqlDocCategory.category)\
+            .order_by(SqlDocCategory.order_num.asc())\
+            .all()
+
+    for mcat in main_cats:
+        category_list.append({"Title": mcat.category, "Id": mcat.id, "Tree": 'main'})
+
+        # main 카테고리에 글이 있는지 확인하고 있으면 리스트에 추가
+        docs = SqlDoc.query\
+                .filter_by(category_id=mcat.id)\
+                .with_entities(SqlDoc.id, SqlDoc.title)\
+                .order_by(SqlDoc.order_num.asc())\
+                .all()
+        
+        if len(docs) > 0:
+            for doc in docs:
+                category_list.append({"Title": doc.title, "Id": doc.id, "Tree": 'doc'})
+
+        # main 카테고리에 sub 카테고리가 있는지 확인하고 있으면 리스트에 추가
+        sub_cats = SqlDocCategory.query\
+                .filter_by(parent_id=mcat.id)\
+                .with_entities(SqlDocCategory.id, SqlDocCategory.category)\
+                .order_by(SqlDocCategory.order_num.asc())\
+                .all()
+  
+        if len(sub_cats) > 0:
+            for scat in sub_cats:
+                category_list.append({"Title": scat.category, "Id": scat.id, "Tree": 'sub'})
+                docs = SqlDoc.query\
+                    .filter_by(category_id=scat.id)\
+                    .with_entities(SqlDoc.id, SqlDoc.title)\
+                    .order_by(SqlDoc.order_num.asc())\
+                    .all()
+
+                for doc in docs:
+                    category_list.append({"Title": doc.title, "Id": doc.id, "Tree": 'doc'})
+
+    return jsonify({
+        "categories": category_list
+    }), 200
+
+        
